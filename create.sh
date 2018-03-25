ab13b6fb05992b8f7820115f405f2a8c70b806b7
Yuji Imamura <YI201610@users.noreply.github.com>
2018-03-25 14:26:32 +0900

    検証項目フォルダ作成スクリプトを追加

---
+++ create.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+if [ $# -ne 1 ]; then
+    echo "usage: ./create.sh <検証項目名>"
+    exit
+fi
+
+# 注意: =と引数の間にスペースを入れないこと
+export itemName=$1
+
+mkdir ${itemName}
+cd ${itemName}
+
+touch README.md
+
+echo "[✓] directory: ${itemName} created."
+
+exit
+

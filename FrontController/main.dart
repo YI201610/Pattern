ac0db9c3c73823ad59be52c18d31e26c9d0f0f5b
Yuji Imamura <YI201610@users.noreply.github.com>
2018-04-07 07:52:41 +0900

    開発中

--- FrontController/main.dart
+++ FrontController/main.dart
@@ -3,6 +3,9 @@ import 'FrontHandler.dart';
 main() {
   var front_controller = new FrontController();
 
+  Uri sampleUri = Uri("https://sample-domain.com", "userInfo", )
+  front_controller.analyze(url: )
+
   print(front_controller);
 
 }

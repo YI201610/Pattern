8aae3480c4a15e8b424fca15d50b94d1a860148b
Yuji Imamura <YI201610@users.noreply.github.com>
2018-04-07 17:01:31 +0900

    サンプルクラス追加

--- TransactionScript/lib/Domain/TransactionScript3.dart
+++ TransactionScript/lib/Domain/TransactionScript3.dart
@@ -1,6 +1,6 @@
 import 'package:TransactionScript/TechnicalService/DBService.dart';
 
-class TransactionScript1 {
+class TransactionScript3 {
 
   run(){
     DBService.query("sql");
---
+++ TransactionScript/lib/TechnicalService/DBService.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+class DBService {
+
+  static Future<Map> query(String sql) {
+
+    Future<Map> result;
+    return result;
+
+  }
+
+}

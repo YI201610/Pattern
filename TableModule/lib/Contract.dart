8fe62148fafabc5e417f2d58e86bc6b2117ea2cd
Yuji Imamura <YI201610@users.noreply.github.com>
2018-04-10 06:38:45 +0900

    Contractのコンストラクタ実装中

--- TableModule/lib/Contract.dart
+++ TableModule/lib/Contract.dart
@@ -1,5 +1,10 @@
 import 'package:TableModule/TableModule.dart';
+import 'package:TableModule/DataSet.dart';
 
 class Contract extends TableModule {
 
+  Contract(DataSet dataSet, String tableName) {
+    super(dataSet, tableName);
+
+  }
 }
--- TableModule/main.dart
+++ TableModule/main.dart
@@ -1,5 +1,8 @@
-
+import 'package:TableModule/DataSet.dart';
+import 'package:TableModule/Contract.dart';
 
 main() {
+  DataSet hoge;
+  Contract a = new Contract(hoge, 'hoge');
   print('done.');
 }
